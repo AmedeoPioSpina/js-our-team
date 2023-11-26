043f65d08b98fbc3d3e24af01ba0dd1742744c54
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-26 16:02:11 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const users = [
+    {
+        fullName: "Wayne Barnett",
+        job: "Founder & CEO",
+        referenceImage: "../img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        fullName: "Angela Caroll",
+        job: "Chief Editor",
+        referenceImage: "../img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        fullName: "Walter Gordon",
+        job: "Office Manger",
+        referenceImage: "../img/walter-gordon-office-manager.jpg"
+    },
+    {
+        fullName: "Angela Lopez",
+        job: "Social Media Manager",
+        referenceImage: "../img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        fullName: "Scott Estrada",
+        job: "Developer",
+        referenceImage: "../img/scott-estrada-developer.jpg"
+    },
+    {
+        fullName: "Barbara Ramos",
+        job: "Graphic Designer",
+        referenceImage: "../img/barbara-ramos-graphic-designer.jpg"
+    },
+]
+
+console.log(users);
